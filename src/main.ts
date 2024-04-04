42e8d26492483495f8936d40914fedf92f5576e2
Pavel Vinogradov <p.vinogradov@sellerexpert.ru>
2024-04-04 21:07:03 +0300

    Добавлен тултип

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { createPinia } from 'pinia';
 import { router } from '@/router';
 import '@/style.scss';
 const pinia = createPinia();
+import FloatingVue from 'floating-vue';
+import 'floating-vue/dist/style.css';
 import App from '@/App.vue';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 const app = createApp(App);
 app.use(router);
 app.use(pinia);
+app.use(FloatingVue);
 app.mount('#app');
