e4746bc7faecfb9c728ff0c5c89e108e239636cd
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2024-11-24 22:27:40 +0300

    Сделаны небольшие фиксы с помощью ESLint

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,21 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }: { mode: string }) => {
-    return {
-        plugins: [vue()],
-        base: '/aphorisms/',
-        resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-        server: {
-            proxy: {
-                '/api': {
-                    target: 'https://focusinfo.ru/aphorisms/api',
-                    changeOrigin: true,
-                    rewrite: (path: string) => path.replace(/^\/api/, ''),
-                },
+export default defineConfig(({ mode }: { mode: string }) => ({
+    plugins: [vue()],
+    base: '/aphorisms/',
+    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://focusinfo.ru/aphorisms/api',
+                changeOrigin: true,
+                rewrite: (path: string) => path.replace(/^\/api/, ''),
             },
         },
-        css: {
-            modules: { generateScopedName: mode === 'development' ? '' : '[hash:base64:8]' },
-            preprocessorOptions: { scss: { api: 'modern-compiler' } }, 
-        },
-    };
-});
+    },
+    css: {
+        modules: { generateScopedName: mode === 'development' ? '' : '[hash:base64:8]' },
+        preprocessorOptions: { scss: { api: 'modern-compiler' } },
+    },
+}));
