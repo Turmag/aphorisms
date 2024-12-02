3280781318e3dccfb9872b7f19d256f08df4adcf
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2024-12-03 00:13:43 +0300

    Обновлён пакет turmag-eslint-config, удалены лишние

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,13 @@
 import config from 'turmag-eslint-config';
-import specialRules from 'eslint-plugin-turmag-special-rules';
 import { aliases } from './aliases.mjs';
 
 export default [
-    { plugins: { 'special-rules': specialRules } },
-
     ...config,
 
     {
         rules: {
-            'special-rules/prefer-true-attribute-shorthand': ['error', 'always'],
             'special-rules/add-vue-extension': ['error', { aliases }],
             'special-rules/use-shortest-alias': ['error', { aliases }],
-            'special-rules/import-entities-by-column-or-line': ['error', { minProperties: 3 }],
             'special-rules/import-right-order': 'off',
         },
     },
