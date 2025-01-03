0ec44e190ef0c7ddc80171831a6d0bac3ba7626e
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2025-01-03 22:58:59 +0300

    Небольшая правка по формату передачи для AuthApi

--- src/shared/api/AuthApi.ts
+++ src/shared/api/AuthApi.ts
@@ -3,16 +3,12 @@ import axios, { type AxiosPromise } from 'axios';
 const path = import.meta.env.MODE === 'development' ? '' : import.meta.env.BASE_URL;
 
 export default {
-    authorize: (password: string): AxiosPromise<string> => {
-        const formData = new FormData();
-        formData.append('password', password);
-        return axios.post(`${path}/api/authorize.php`, formData, {
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'text/html',
-            },
-        });
-    },
+    authorize: (password: string): AxiosPromise<string> => axios.post(`${path}/api/authorize.php`, { password }, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'text/html',
+        },
+    }),
     checkAuthorize: () => axios.post<string>(`${path}/api/checkAuthorize.php`),
     logout: () => axios.post<string>(`${path}/api/logout.php`),
 };
