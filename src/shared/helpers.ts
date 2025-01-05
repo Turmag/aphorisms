a1de03c51bf85584905db3417743c7a97c0d3f0d
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2025-01-06 02:30:23 +0300

    Методу getSecondsDiff добавлен возвращемый тип

--- src/shared/helpers.ts
+++ src/shared/helpers.ts
@@ -1,4 +1,4 @@
-export const getSecondsDiff = (d1: Date, d2: Date) => {
+export const getSecondsDiff = (d1: Date, d2: Date): number => {
     const diff = (d2.getTime() - d1.getTime()) / 1000;
     return Math.abs(Math.round(diff));
 };
