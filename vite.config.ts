954314d7f6ab6877c6ee44b37930a69caa1d70ba
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2025-01-16 20:29:04 +0300

    Обновлён пакет sass и подкорректирован файл vite.config.ts - сделана сортировка

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: { mode: string }) => ({
-    plugins: [vue()],
     base: '/aphorisms/',
+    css: {
+        modules: { generateScopedName: mode === 'development' ? '' : '[hash:base64:8]' },
+        preprocessorOptions: { scss: { api: 'modern-compiler' } },
+    },
+    plugins: [vue()],
     resolve: { alias: { '@': path.resolve(__dirname, './src') } },
     server: {
         proxy: {
             '/api': {
-                target: 'https://focusinfo.ru/aphorisms/api',
                 changeOrigin: true,
                 rewrite: (path: string) => path.replace(/^\/api/, ''),
+                target: 'https://focusinfo.ru/aphorisms/api',
             },
         },
-    },
-    css: {
-        modules: { generateScopedName: mode === 'development' ? '' : '[hash:base64:8]' },
-        preprocessorOptions: { scss: { api: 'modern-compiler' } },
     },
 }));
