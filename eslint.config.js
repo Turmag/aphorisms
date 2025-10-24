f8f9729a2fb08fd3d77c45b9fa875e94315ad736
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2025-10-24 18:57:15 +0300

    chore: main Установлен и базово настроен линтер oxlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,16 @@
 import { aliases } from './aliases.js';
+import pluginOxlint from 'eslint-plugin-oxlint';
 import config from 'turmag-eslint-config';
 
 export default [
     ...config,
+    ...pluginOxlint.configs['flat/recommended'],
 
     {
         rules: {
             'special-rules/add-vue-extension': ['error', { aliases }],
             'special-rules/use-shortest-alias': ['error', { aliases }],
+            'oxlint/no-unused-expressions': ['off'],
         },
     },
 ];
